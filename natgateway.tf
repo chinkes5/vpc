9a7ba4cb84cb4a576d19770bd083cae5bfa7587c
John Chinkes <john@chinkes.com>
2021-10-18 15:40:09 -0600

    typo fix

--- natgateway.tf
+++ natgateway.tf
@@ -2,7 +2,7 @@ resource "aws_nat_gateway" "natgateway" {
   allocation_id = "aws_eip.ip.id"
   subnet_id     = "aws_subnet.publicsubnet.id"
   tags = {
-    name = "prod nategatway"
+    name = "prod natgateway"
   }
   depends_on = [aws_eip.eip]
 }
